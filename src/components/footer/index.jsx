8ba7568ac20a47853b39e6c8261c0c10aea8bfd8
40 daimon <tvwmdaimon40@gmail.com>
2020-09-28 10:55:09 -0700

    finish footer components

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-import { Container, Row, Column, Link, Text, Break } from './styles/footer';
+import {
+  Container,
+  Row,
+  Column,
+  Link,
+  Title,
+  Text,
+  Break,
+} from './styles/footer';
 
 export default function Footer({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
