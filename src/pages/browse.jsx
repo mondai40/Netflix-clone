a75a88481c9db868b07a8403c60726b562a75754
40 daimon <tvwmdaimon40@gmail.com>
2020-09-29 20:14:58 -0700

    create useContent which retrieve films or series data

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import { useContent } from '../hooks';
+
 export default function Browse() {
   // we need the series and the films
+  const { series } = useContent('series');
+  const { films } = useContent('films');
   // we need slides
   // pass it to the browse container
 
