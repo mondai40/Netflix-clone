aa5e53fd5d7bfcf33388b163b385f8557ef7ec67
40 daimon <tvwmdaimon40@gmail.com>
2020-10-05 09:55:16 -0700

    finish header test

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -22,7 +22,13 @@ import {
 } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
-  return bg ? <Background {...restProps}>{children}</Background> : children;
+  return bg ? (
+    <Background {...restProps} data-testid="header-bg">
+      {children}
+    </Background>
+  ) : (
+    children
+  );
 }
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
@@ -61,6 +67,7 @@ Header.FeatureCallOut = function HeaderFeatureCallOut({
 };
 
 Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  console.log('in link', restProps.active);
   return <Link {...restProps}>{children}</Link>;
 };
 
@@ -86,6 +93,7 @@ Header.Search = function HeaderSearch({
     <Search {...restProps}>
       <SearchIcon
         onClick={() => setSearchActive((searchActive) => !searchActive)}
+        data-testid="search-click"
       >
         <img src="/images/icons/search.png" alt="Search" />
       </SearchIcon>
@@ -94,6 +102,7 @@ Header.Search = function HeaderSearch({
         onChange={({ target }) => setSearchTerm(target.value)}
         placeholder="Search films and series"
         active={searchActive}
+        data-testid="search-input"
       />
     </Search>
   );
